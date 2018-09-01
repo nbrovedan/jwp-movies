c8671f1aa6714c80b021a768408165c6eddfb6e9
Naidion Brovedan <naidion@MacBook-Pro-de-Naidion.local>
2018-09-01 10:47:05 -0300

    First message

---
+++ src/main/java/br/com/movies/JwpMoviesSb2Application.java
@@ -0,0 +1,12 @@
+package br.com.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpMoviesSb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpMoviesSb2Application.class, args);
+	}
+}
---
+++ src/test/java/br/com/movies/JwpMoviesSb2ApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.movies;
+
+
+/*@RunWith(SpringRunner.class)
+@Test
+public class JwpMoviesSb2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}*/
